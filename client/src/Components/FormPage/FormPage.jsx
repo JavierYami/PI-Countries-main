c387faed623349bfa95ee64a63b554d0d4a840f3
JavierYami <javieyami1@outlook.com>
2023-03-26 16:59:34 -0600

    prototipado de form beta

--- client/src/Components/FormPage/FormPage.jsx
+++ client/src/Components/FormPage/FormPage.jsx
@@ -9,17 +9,64 @@ const FormPage = () => {
     const countries = useSelector((state) => state.countries)
 
     const [activity, setActivity] = useState({
-
+        name: '',
+        difficulty: 0,
+        duration : 0,
+        season: '',
     })
 
+    const handleOnChange = (event) => {
+        const value = event.target.value;
+        const name = event.target.name;
+        setActivity({
+            ...activity,
+            [name] : value,
+        })
+    }
+
     return (
+
         <form>
-            <select>
+
+            <label>Name: </label>
+            <input type="text" name="name" value={activity.name} onChange={handleOnChange}/>
+
+            <label>Duration: </label>
+            <input type="text" name="duration" value={activity.duration} onChange={handleOnChange} />
+
+            <label>Difficulty: </label>
+            <select name="difficulty" onChange={handleOnChange}>
+                <option value={0}>Select a difficulty</option>
+                <option value={1}>Easy</option>
+                <option value={2}>Moderate</option>
+                <option value={3}>Indermediate</option>
+                <option value={4}>Hard</option>
+                <option value={5}>Extreme</option>
+            </select>
+
+            <label>Season: </label>
+            <select name="season" onChange={handleOnChange}>
+                <option value="0">Select a season</option>
+                <option value="Summer">Summer</option>
+                <option value="Winter">Winter</option>
+                <option value='Spring'>Spring</option>
+                <option value="Autumn">Autumn</option>
+            </select>
+
+
+            <label >Countries: </label>
+            <select name="countries">
+                <option value="0">Select a country</option>
                 {countries.map((country) => {
-                    return <option value={country.id}>{country.name}</option>
+                    return <option value={country.id} key={country.id}>{country.name}</option>
                 })}
             </select>
+
+
+            <input type="submit" value="Enviar"></input>
+
         </form>
+
     )
 
 }
