f14ffa99a71739578a74a9956219837170c70429
JavierYami <javieyami1@outlook.com>
2023-03-26 15:25:46 -0600

    prototipado del form

--- client/src/Components/FormPage/FormPage.jsx
+++ client/src/Components/FormPage/FormPage.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 
+import { useDispatch, useSelector } from "react-redux";
 
 const FormPage = () => {
-    
+
+    const dispatch = useDispatch();
+
+    const countries = useSelector((state) => state.countries)
+
+    const [activity, setActivity] = useState({
+
+    })
+
     return (
-        <h1>Create Activity</h1>
+        <form>
+            <select>
+                {countries.map((country) => {
+                    return <option value={country.id}>{country.name}</option>
+                })}
+            </select>
+        </form>
     )
 
 }
