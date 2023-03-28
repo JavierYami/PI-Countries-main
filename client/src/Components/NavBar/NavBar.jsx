e2f6ccccf8f8e9b72a1f3b309cddb5c547522185
JavierYami <javieyami1@outlook.com>
2023-03-28 15:51:08 -0600

    stilos nav

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,11 +1,12 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import style from "../NavBar/NavBar.module.css"
 
 const NavBar = () => {
 
     return ( 
-        <nav>
-            <Link to={'homepage'}><button>Home</button></Link>
-            <Link to={'create'}><button>Create Activity</button></Link>
+        <nav className={style.nav}>
+            <Link to={'homepage'}><button className={style.button}>Home</button></Link>
+            <Link to={'create'}><button className={style.button}>Create Activity</button></Link>
         </nav>
     )
 }
