259810556cca7cdf0e07124cd201aa79650c8f77
JavierYami <javieyami1@outlook.com>
2023-03-28 18:04:00 -0600

    landing css

--- client/src/Components/LandingPage/LandingPage.jsx
+++ client/src/Components/LandingPage/LandingPage.jsx
@@ -1,12 +1,11 @@
 import {Link} from 'react-router-dom'
-
 import style from './LandingPage.module.css'
 
 export default function LandingPage (props) {
     return (
-        <div>
+        <div className={style.container}>
             <h1>Countries</h1>
-            <Link to='homepage'><h4>Explore the world</h4></Link>
+            <Link to='homepage'><button className={style.button}>Explore the world</button></Link>
         </div>
 
     )
